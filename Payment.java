a204f38742754b764b527d0b70724b184aafce3f
XCX <42567504+XueChuanxu@users.noreply.github.com>
2019-10-23 08:46:17 +0800

    Add files via upload

---
+++ Payment.java
@@ -0,0 +1,92 @@
+package com.company.Entity;
+
+/**
+ * Represents a payment during movie ticket booking
+ * Payment contains the information of transactionID,
+ * name, mobile number and email of payer, and
+ * amount of this payment
+ * @author SSP2 Team 1
+ */
+public class Payment {
+    /**
+     * The transactionID of this Payment
+     */
+    private String transactionID;
+    /**
+     * The name of the payer
+     */
+    private String name;
+    /**
+     * The mobile number of the payer
+     */
+    private String mobileNumber;
+    /**
+     * The email of the payer
+     */
+    private String email;
+    /**
+     * The amount of this Payment
+     */
+    private double paymentAmount;
+    /**
+     * Creates a new Payment with given
+     * transactionID of this Payment,
+     * name, mobile number and email of payer, and
+     * amount of this payment
+     * @param transactionID The transactionID of this Payment
+     * @param mobileNumber The mobile number of the payer
+     * @param name The name of the payer
+     * @param email The email of the payer
+     * @param paymentAmount The paymentAmount of this payment
+     */
+    public Payment(String transactionID, String name, String mobileNumber, String email, double paymentAmount) {
+        this.transactionID = transactionID;
+        this.name = name;
+        this.mobileNumber = mobileNumber;
+        this.email = email;
+        this.paymentAmount = paymentAmount;
+    }
+    /**
+     * Gets the transaction ID of this payment
+     * @return the transaction ID of this payment
+     */
+    public String getTransactionID() {
+        return transactionID;
+    }
+    /**
+     * Gets the name of the payer
+     * @return the name of the payer
+     */
+    public String getName() {
+        return name;
+    }
+    /**
+     * Gets the mobile number of the payer
+     * @return the mobile number of the payer
+     */
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+    /**
+     * Gets the email of the payer
+     * @return the email of the payer
+     */
+    public String getEmail() {
+        return email;
+    }
+    /**
+     * Gets the paymentAmount of this payment
+     * @return the payment amount of this payment
+     */
+    public double getPaymentAmount() {
+        return paymentAmount;
+    }
+    /**
+     * Gets the string representation
+     * for all the infomation about this payment
+     * @return string representation about this payment
+     */
+    public String toString() {
+        return "TransactionID: " + transactionID + "\nName: " + name + "\nMobile Number: " + mobileNumber + "\nEmail: " + email + "\nPayment amount: " + paymentAmount + "SGD";
+    }
+}
